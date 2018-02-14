b3e9923cbbafb98742da52eabc54c372e0b19419
Holly Wilkalis and Sailor Winkelman <holly@>
2018-02-14 13:43:31 -0800

    Create configuration files for npm, webpack, and linter. Create entry point. Create App component.

---
+++ src/components/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+// import { Switch, Route } from 'react-router-dom';
+
+
+function App() {
+  var appStyle = {
+    boxSizing: 'border-box',
+    margin: '0',
+    backgroundColor: '#FFFAFA',
+    color: '#EB6EBD',
+    fontFamily: 'sans-serif',
+    fontSize: '18px'
+  };
+  return (
+    <div style={appStyle}>Tamagotchi</div>
+  );
+}
+
+
+
+export default App;
